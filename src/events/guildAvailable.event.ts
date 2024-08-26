eb428747ec9659d0b087c42951a45348cd69824f
Carol Diniz <CarolDinizc21@gmail.com>
2024-08-26 06:58:38 -0300

    add guild in db and deploy commands
    Add a initialization to guild in dabatase and a deploy commads

--- src/events/guildAvailable.event.ts
+++ src/events/guildAvailable.event.ts
@@ -1,5 +1,26 @@
 import { Guild } from "discord.js";
+import { logger } from "..";
+import { deployCommands } from "../deployCommands";
+import { ModelGuild } from "../models/guild.model";
 
-export function guildAvailableEvent(guild: Guild) {
+export async function guildAvailableEvent(guild: Guild) {
+  try {
+    logger.info(`Guild connected: ${guild.name}`);
+    await initializeGuildInDatabase(guild);
+    await deployCommands(guild.id);
+  } catch (error) {
+    logger.error("Error deploying commands to guild:", error);
+    return null;
+  }
+}
 
+async function initializeGuildInDatabase(guild: Guild) {
+  const guildDb = new ModelGuild({
+    guildId: guild.id,
+    guildName: guild.name,
+    guildOwner: guild.ownerId,
+    categoryCartId: null,
+  });
+  await guildDb.save();
+  logger.info(`Guild (${guild.name}) saved in database`);
 }
