7677c0dd83f769ce29e4f43453d0a27b1cffb7a5
Carol Diniz <CarolDinizc21@gmail.com>
2024-08-26 01:59:34 -0300

    remove a necessidade de importação do dotenv/config

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import { connect, connection, ConnectOptions, disconnect } from "mongoose";
 
 const uri = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@chargeautomation.nevu6ex.mongodb.net/?retryWrites=true&w=majority&appName=ChargeAutomation`;
@@ -11,10 +10,10 @@ export async function database() {
     console.log("Connecting to MongoDB...");
     await connect(uri, clientOptions);
     await connection.db?.admin().command({ ping: 1 });
-    console.log("You successfully connected to MongoDB!")
+    console.log("You successfully connected to MongoDB!");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1)
+    process.exit(1);
   } finally {
     await disconnect();
   }
