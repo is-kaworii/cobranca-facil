6b21a90382587f3ce9cdd214d22653b555e284a8
Carol Diniz <CarolDinizc21@gmail.com>
2024-08-26 06:56:35 -0300

    add application id  from global var client

--- src/deployCommands.ts
+++ src/deployCommands.ts
@@ -1,15 +1,16 @@
 import { REST, Routes } from "discord.js";
-import { logger } from ".";
+import { client, logger } from ".";
 import { commands } from "./commands/commands";
 import { exit } from "./utils/exit";
 
 const rest: REST = new REST({ version: "10" }).setToken(process.env.applicationToken!);
 const commandsData = Object.values(commands).map((command) => command.data);
 
-export async function deployCommands(guildId: string, clientId: string) {
+export async function deployCommands(guildId: string) {
   try {
     logger.info("Deploying commands")
-    let data: any = await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+    const applicationId = client.user!.id
+    let data: any = await rest.put(Routes.applicationGuildCommands(applicationId, guildId), {
       body: commandsData,
     });
     logger.info(`${data.length} Commands successfully deployed`)
